6ea09ae04f9586d14f1c281fc273781778e2efad
Michael Rhöse <michael.rhoese@fh-erfurt.de>
2017-03-23 10:28:33 +0100

    Comments changed

--- src/test/java/io/varietas/agrestis/imputare/searching/ClassCollectorTest.java
+++ src/test/java/io/varietas/agrestis/imputare/searching/ClassCollectorTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Michael Rhöse.
+ * Copyright 2016 varietas.io
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
